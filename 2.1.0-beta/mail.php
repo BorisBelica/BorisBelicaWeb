e075afbbb3e93ae136ba13d7b0584a1cf60e0d2f
Boris <boris.belica2@gmail.com>
2020-08-26 23:59:07 +0200

    ver. 2.1.0-beta
    - added new footer,
    - responsive,
    - small nav bar edits.

---
+++ 2.1.0-beta/mail.php
@@ -0,0 +1,30 @@
+<?php
+
+$spam_ochrana = "sirob";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$ochrana = $_POST['ochrana'];
+$message = $_POST['message'];
+
+
+if ($spam_ochrana == $ochrana) {
+	
+	$formcontent="$name \nEmail: $email \n \nSpráva:\n $message";
+	$recipient = "boris.belica2@gmail.com";
+	$subject = "Kontatkný formulár - Boris Belica Web";
+
+	$mailheader = "Od: $email \r\n";
+
+	mail($recipient, $subject, $formcontent, $mailheader) or die("CHYBA! Správa nebola odoslaná!");
+	echo "Ďakujeme za Vašu správu!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+	
+}
+
+else {
+	
+	echo "Spam ochrana!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+	
+}
+
+?>
